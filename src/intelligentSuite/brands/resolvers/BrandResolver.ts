82a309f8bbea734d09edad2f990d36a1ebae3d42
Gonzalo Gregorio <gonzagregorio03@gmail.com>
2023-08-15 19:48:43 -0300

    Add documentation for resolver controllers

--- src/intelligentSuite/brands/resolvers/BrandResolver.ts
+++ src/intelligentSuite/brands/resolvers/BrandResolver.ts
@@ -18,13 +18,26 @@ export class BrandResolver {
         private readonly brandAccountsService: BrandAccountsService,
     ) {}
 
+    /**
+     * Creates a new brand associated with the provided user admin.
+     * @param user - The authenticated user (admin).
+     * @param input - Brand details for creation.
+     * @returns Brand The newly created brand.
+     */
     @Mutation((_returns) => Brand, {
-        description: "Creates a business Account for the provided business admin",
+        description: "Creates a business Account for the provided user admin",
     })
     async createBrand(@CurrentUser() user: User, @Arg("input") input: CreateBrandInput) {
         return await this.brandService.createBrand(user, input);
     }
 
+    /**
+     * Updates optional fields of a brand.
+     * @param user - The authenticated user (admin).
+     * @param brandId - The ID of the brand to update.
+     * @param input - Updated brand details.
+     * @returns Brand The updated brand.
+     */
     @Mutation((_returns) => Brand, {description: "Updates optional fields of a brand"})
     async updateBrand(
         @CurrentUser() user: User,
@@ -34,6 +47,13 @@ export class BrandResolver {
         return await this.brandService.updateBrand(user, brandId, input);
     }
 
+    /**
+     * Updates assets of a brand.
+     * @param user - The authenticated user (admin).
+     * @param brandId - The ID of the brand to update assets.
+     * @param input - Updated brand account assets' details.
+     * @returns Brand The brand with updated assets.
+     */
     @Mutation((_returns) => Brand, {description: "Updates assets of a brand"})
     async updateBrandAssets(
         @CurrentUser() user: User,
@@ -43,6 +63,13 @@ export class BrandResolver {
         return await this.brandAccountsService.updateBrandAssets(user, brandId, input);
     }
 
+    /**
+     * Updates the status of a brand.
+     * @param user - The authenticated user (admin).
+     * @param brandId - The ID of the brand to update the status for.
+     * @param input - New status for the brand. Possible values: IN_PROGRESS, DATA_READY, MODEL_TRAINING, READY
+     * @returns Brand The brand with updated status.
+     */
     @Mutation((_returns) => Brand, {description: "Updates status of a brand"})
     async updateBrandStatus(
         @CurrentUser() user: User,
@@ -52,11 +79,23 @@ export class BrandResolver {
         return await this.brandService.updateBrandStatus(user, brandId, input);
     }
 
+    /**
+     * Get upload data for brand logo.
+     * @param user - The authenticated user.
+     * @param input - Upload request details.
+     * @returns UploadDataResponse Logo upload information.
+     */
     @Mutation((_returns) => UploadDataResponse, {description: "Uploads brand logo"})
     async requestLogoUploadData(@CurrentUser() user: User, @Arg("input") input: UploadRequestInput) {
         return await this.brandService.getLogoUploadData(user, input);
     }
 
+    /**
+     * Retrieves brand assets available in integration for a business account.
+     * @param user - The authenticated user.
+     * @param brandId - The ID of the brand to retrieve assets for.
+     * @returns BrandAssetsResponse Brand assets containing available social or ad accounts.
+     */
     @Query((_returns) => BrandAssetsResponse, {
         description: "Returns the Business Asset available in integration for business Account",
     })
